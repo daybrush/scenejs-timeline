56010e712018386d90a78547f4f55b2cb64ec61a
daybrush <daybrush@gmail.com>
2021-02-13 22:12:16 +0900

    feat: add cursor click

--- packages/react-scenejs-timeline/src/react-scenejs-timeline/areas/HeaderArea.tsx
+++ packages/react-scenejs-timeline/src/react-scenejs-timeline/areas/HeaderArea.tsx
@@ -6,6 +6,7 @@ import { ref } from "framework-utils";
 import Timeline from "../Timeline";
 import Ruler from "@scena/react-ruler";
 import Gesto from "gesto";
+import { throttle } from "@daybrush/utils";
 
 export default class HeaderArea extends React.PureComponent<{
     timelineInfo: TimelineInfo | null,
@@ -14,6 +15,7 @@ export default class HeaderArea extends React.PureComponent<{
 }> {
     public ruler!: Ruler;
     public cursorElement!: HTMLElement;
+    public keyframesAreaElement!: HTMLElement;
     public state = {
         scrollLeft: -0.1,
     };
@@ -31,7 +33,9 @@ export default class HeaderArea extends React.PureComponent<{
                 <div className={prefix("properties-area")}>
                     <div className={prefix("property")}>Name</div>
                 </div>
-                <div className={prefix("keyframes-area")}>
+                <div
+                    ref={ref(this, "keyframesAreaElement")}
+                    className={prefix("keyframes-area")}>
                     <Ruler
                         ref={ref(this, "ruler")}
                         zoom={zoom}
@@ -58,19 +62,31 @@ export default class HeaderArea extends React.PureComponent<{
     public componentDidMount() {
         this.ruler.scroll(-0.1);
 
-        this._gesto = new Gesto(this.cursorElement, {
+        this._gesto = new Gesto(this.keyframesAreaElement, {
             container: window,
         }).on("dragStart", e => {
+            e.datas.isDrag = e.inputEvent.target === this.cursorElement;
             e.datas.startTime = this.props.timeline.getTime();
         }).on("drag", e => {
+            if (!e.datas.isDrag) {
+                return;
+            }
             const startTime = e.datas.startTime;
             const distTime = e.distX / this.props.zoom;
 
             this.props.timeline.setTime(startTime + distTime);
+        }).on("dragEnd", e => {
+            if (!e.isDrag) {
+                const distX = e.clientX - this.keyframesAreaElement.getBoundingClientRect().left;
+                const distTime = distX / this.props.zoom;
+                const time = throttle(Math.max(0, this.state.scrollLeft + distTime), 0.1);
+
+                this.props.timeline.setTime(time);
+            }
         });
     }
     public scroll(scrollLeft: number) {
-        const zoom = this.props.zoom;
+        // const zoom = this.props.zoom;
         // this.ruler.scroll(scrollLeft - 0.1);
         // this.cursorElement.style.transform = `translate(-50%) translate(calc(var(--scenejs-timeline-time) * ${zoom}px + ${zoom * (0.1 - scrollLeft)}px))`;
         this.setState({
